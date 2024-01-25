43ecf1aed97f6284f72b27c393c83a63b72ee9ac
yatharthsoni <yatharth.soni14@gmail.com>
2024-01-25 12:31:39 -0500

    - Finished Admin Registration page with implementing proper navigation from the login page for setting up the application - Some Minor UI touch ups.

--- app/src/main/java/com/example/laxmijewellersapplication/MainActivity.java
+++ app/src/main/java/com/example/laxmijewellersapplication/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.laxmijewellersapplication;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     private Button loginButton;
     private TextView forgotPassword;
+    private LinearLayout adminRegistration;
 
 
     @Override
@@ -28,6 +31,14 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, DashboardActivity.class));
             }
 
+        });
+
+        adminRegistration = findViewById(R.id.adminRegistration);
+        adminRegistration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, RegisterUserActivity.class));
+            }
         });
 
 
